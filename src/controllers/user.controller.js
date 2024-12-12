1e360ce50c190b36dc1aa72f7110555655650628
Navdeep Singh <84785739+Navdeepannu@users.noreply.github.com>
2024-12-12 09:51:01 -0500

    complete user registration controller & fix related bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,85 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary_service.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  
-    
+  // Get User details
+  const { fullName, email, username, password } = req.body;
+  //console.log("email : ", email);
+
+  // Validations
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required!");
+  }
+
+  // Check for existing user
+  const existingUser = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (existingUser) {
+    throw new ApiError(409, "User with this email or usename already exists!");
+  }
+
+  //TODO: Check the files...
+  //console.log("req.files : ", req.files);
+
+  // file handling
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // TODO:
+  // coverImage is not required from the user, needs a different check
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0 
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  // Avatar File requirement check
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // Upload to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  // Check for avatar
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // Create user in database
+  const user = await User.create({
+    fullName,
+    email,
+    username: username.toLowerCase(),
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  // check if user is created
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  // Return response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
